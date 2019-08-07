c7cbb6b6fa046609c3465a004c3d70520f49ccce
Manoel Afonso <mano.afonso93@gmail.com>
2019-08-07 14:33:39 -0300

    Fix discount amount

--- src/Exercicio01.java
+++ src/Exercicio01.java
@@ -12,17 +12,22 @@ public class Exercicio01 {
     public static void main(String[] args) {
 
         Scanner teclado = new Scanner(System.in);
+
         double valor;
         double desconto;
         double valorDesconto;
+
         System.out.println("Digite o valor do Produto");
         valor = teclado.nextDouble();
+
         System.out.println("Digite o valor do desconto");
         desconto = teclado.nextDouble();
+
         valorDesconto = valor * desconto / 100;
+        
         System.out.println("Valor do produto " + valor);
         System.out.println("Valor do desconto " + desconto);
-        System.out.println("Valor do produto com desconto " + (valor - desconto));
+        System.out.println("Valor do produto com desconto " + (valor - valorDesconto));
 
     }
 }
